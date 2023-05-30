c80552da69c0cdc79536c1eda2a69790fc7bff44
victor-ivanov-ivt20-2 <72002526+victor-ivanov-ivt20-2@users.noreply.github.com>
2023-05-31 08:00:03 +0900

    choose note

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./views/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        wnote: "inset 0px -2px 0px 2px rgba(0, 0, 0, 0.25)",
+        bnote: "inset 0px -3px 0px 3px rgba(255, 255, 255, 0.15)",
+      },
+    },
   },
   plugins: [],
 };
